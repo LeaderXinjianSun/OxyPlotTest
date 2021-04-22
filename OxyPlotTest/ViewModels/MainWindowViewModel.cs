e7bedd4ed376f7a78b238e89534a8bfe5982d969
sunxinjian <715054307@qq.com>
2021-04-22 09:41:12 +0800

    添加项目文件。

---
+++ OxyPlotTest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using OxyPlot;
+using Prism.Mvvm;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace OxyPlotTest.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+        private IList<DataPoint> leftPoints;
+
+        public IList<DataPoint> LeftPoints
+        {
+            get { return leftPoints; }
+            set
+            {
+                leftPoints = value;
+                this.RaisePropertyChanged("LeftPoints");
+            }
+        }
+        public MainWindowViewModel()
+        {
+            LeftPoints = new ObservableCollection<DataPoint>();
+            for (int i = 0; i < 1000; i++)
+            {
+                LeftPoints.Add(new DataPoint(i, i * i));
+            }
+        }
+    }
+}
