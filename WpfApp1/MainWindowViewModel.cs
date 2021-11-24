1558a24a7acd64742c326a97f8ac69885bf31f81
sunxinjian <715054307@qq.com>
2021-11-24 13:37:51 +0800

    1

---
+++ WpfApp1/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Practices.Prism.ViewModel;
+using OxyPlot;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class MainWindowViewModel : NotificationObject
+    {
+        private IList<DataPoint> leftPoints;
+
+        public IList<DataPoint> LeftPoints
+        {
+            get { return leftPoints; }
+            set
+            {
+                leftPoints = value;
+                this.RaisePropertyChanged("LeftPoints");
+            }
+        }
+        public MainWindowViewModel()
+        {
+            LeftPoints = new ObservableCollection<DataPoint>();
+            for (int i = 0; i < 1000; i++)
+            {
+                LeftPoints.Add(new DataPoint(i, i * i));
+            }
+        }
+    }
+}
